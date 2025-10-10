68f0ff79d9be387fda3f161f50c730fe7d1a79c9
Serhii Taran <serg.taran1970@gmail.com>
2025-10-10 21:50:22 +0300

    add-homework

---
+++ src/js/render-functions.js
@@ -0,0 +1,52 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const galleryContainer = document.querySelector('.gallery');
+const loader = document.querySelector('.loader');
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionsDelay: 250,
+});
+
+export function createGallery(images) {
+  const markup = images
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => `
+      <li class="gallery-item">
+        <a href="${largeImageURL}">
+          <img src="${webformatURL}" alt="${tags}" loading="lazy" />
+        </a>
+        <div class="info">
+          <p><span>${likes}</span>Likes</p>
+          <p><span>${views}</span>Views</p>
+          <p><span>${comments}</span>Comments</p>
+          <p><span>${downloads}</span>Downloads</p>
+        </div>
+      </li>`
+    )
+    .join('');
+
+  galleryContainer.insertAdjacentHTML('beforeend', markup);
+  lightbox.refresh();
+}
+
+export function clearGallery() {
+  galleryContainer.innerHTML = '';
+}
+
+export function showLoader() {
+  loader.classList.remove('hidden');
+}
+
+export function hideLoader() {
+  loader.classList.add('hidden');
+}
