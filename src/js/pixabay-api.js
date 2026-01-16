4ab4f751b9e66008189a577147d6d138b4c6a91e
Sergii Taran <serg.taran1970@gmail.com>
2026-01-16 11:01:51 +0200

    add html markup homework-11

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -2,24 +2,21 @@ import axios from 'axios';
 
 const BASE_URL = 'https://pixabay.com/api/';
 const API_KEY = '47040317-0d9018ce78b59a4267667365b';
-const PER_PAGE = 15;
 
-export async function getImagesByQuery(query, page = 1) {
+export function getImagesByQuery(query) {
   const params = {
     key: API_KEY,
     q: query,
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: 'true',
-    page,
-    per_page: PER_PAGE,
   };
 
-  try {
-    const response = await axios.get(BASE_URL, { params });
-    return response.data;
-  } catch (error) {
-    console.error('Error fetching images:', error);
-    throw error;
-  }
+  return axios
+    .get(BASE_URL, { params })
+    .then(response => response.data)
+    .catch(error => {
+      console.error('Error fetching images:', error);
+      throw error;
+    });
 }
